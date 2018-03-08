2cb9695cd573c54f9129f6cb3a40a3768bf9732e
luckygoldenboy9 <37095742+luckygoldenboy9@users.noreply.github.com>
2018-03-08 19:15:09 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,8 +6,9 @@ Modified by Ilyasa
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
+$channelAccessToken = 'ldDWVo+58zIxDawQEIdGbkiitwWcRK28Tv0Cbh3iGyUuLi+Doc6vhIb0OKko51r8CQNNjeXiTuxRkFxxxAbl4o0Ee682KYS9aJAhr0GkNOdmdFv972/E9QyhwcnSd9/AdzG6hA4i43Q+UAoWtvv0LAdB04t89/1O/w1cDnyilFU=
+'; //Your Channel Access Token
+$channelSecret = '5f2bdb476454c1b48a8b26d446e27209';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -34,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '09cf47af-4666-474b-8091-2833fe2495be'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
